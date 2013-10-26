5fbbaea8ca655244c9973c44566bd27088625453
Anders Sveen <anders@f12.no>
2013-10-26 11:18:44 +0200

    Fixed ignores

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ io.sockets.on('connection', function (socket) {
 	
 	// when the client emits 'sendchat', this listens and executes
 	socket.on('sendchat', function (data) {
+		console.log(data)
 		// we tell the client to execute 'updatechat' with 2 parameters
 		io.sockets.in(socket.room).emit('updatechat', socket.username, data);
 	});
